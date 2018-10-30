ad90388973003b146b72404e0b05396b0636ceef
davidtv2008 <davidtv2008@hotmail.com>
2018-10-29 18:29:00 -0700

    updates

---
+++ settings.py
@@ -0,0 +1,13 @@
+# define some colors (R, G, B)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+DARKGREY = (40, 40, 40)
+LIGHTGREY = (100, 100, 100)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+YELLOW = (255, 255, 0)
+
+# game settings
+WIDTH = 800 
+HEIGHT = 600
+FPS = 60
