e4bd906056c72c36d33d73df6b07998ddd7837dc
Michael Perez <mperez980089@yahoo.com>
2018-11-07 14:38:00 -0800

    Made enemies killable

--- koopa.py
+++ koopa.py
@@ -14,60 +14,103 @@ class Koopa(Sprite):
         self.left_walls = left_walls
         self.right_walls = right_walls
 
-        self.image = pygame.image.load('resources/Images/koopa2left.gif')
-        self.rect = self.image.get_rect()
-        self.rect.width = self.rect.width/2
-        self.mask = pygame.mask.from_surface(self.image)
+        # Animation timer stuff
+        self.animation_timer_length = 250
+        self.animation_timer = self.animation_timer_length
+        self.current_animation_frame = 0
+        self.last_frame_ticks = pygame.time.get_ticks()
+        self.delta_time = 0
 
-        self.mask = pygame.mask.from_surface(self.image)
-        self.centerx = self.rect.centerx
-        self.centery = self.rect.centery
-        self.previous_centery = self.centery
+        self.image_frames = [
+            pygame.image.load('resources/Images/koopa1left.gif'),
+            pygame.image.load('resources/Images/koopa2left.gif')
+        ]
+        self.current_image = self.image_frames[self.current_animation_frame]
+        self.rect = self.current_image.get_rect()   # Used for drawing the sprite in the correct location
+
+        self.environment_rect = self.current_image.get_rect()   # Used for detecting floor and wall collisions
+        self.environment_rect.width = self.environment_rect.width * 0.5
+
+        self.top_rect = self.current_image.get_rect()   # Used for mario collisions
+        self.top_rect.height = self.top_rect.height/4
+
+        self.left_rect = self.current_image.get_rect()  # Used for mario collisions
+        self.left_rect.width = self.left_rect.width/4
+        self.left_rect.height = self.left_rect.height/2
+
+        self.right_rect = self.current_image.get_rect() # Used for mario collisions
+        self.right_rect.width = self.right_rect.width/4
+        self.right_rect.height = self.right_rect.height/2
+
+        self.centerx = self.current_image.get_rect().centerx
+        self.centery = self.current_image.get_rect().centery
 
         self.velocity_x = 0.25
         self.velocity_y = 0.1
-        self.gravity = 0.004
+        self.gravity = 0.01
         self.horizontal_speed = 0.25
 
     def update(self):
-        self.previous_centery = self.centery
+        self.update_animation()
 
         self.centerx += self.velocity_x
-
         self.centery += self.velocity_y
         self.velocity_y += self.gravity
 
-        colliding_with_floor = pygame.sprite.spritecollideany(self, self.platform_tops)
-        if colliding_with_floor:
-            self.velocity_y = 0
-            self.centery = self.previous_centery
+        self.rect.center = (self.centerx, self.centery)
+        self.environment_rect.center = (self.centerx, self.centery)
+        self.top_rect.center = (self.centerx, self.centery - 18)
+        self.left_rect.center = (self.centerx - 12, self.centery + 12)
+        self.right_rect.center = (self.centerx + 12, self.centery + 12)
 
-        colliding_with_right_wall = pygame.sprite.spritecollideany(self, self.right_walls)
-        if colliding_with_right_wall:
-            self.velocity_x = self.horizontal_speed
+        for platform_top in self.platform_tops:
+            if self.environment_rect.colliderect(platform_top):
+                self.velocity_y = 0
 
-        colliding_with_left_wall = pygame.sprite.spritecollideany(self, self.left_walls)
-        if colliding_with_left_wall:
-            self.velocity_x = -self.horizontal_speed
+        for left_wall in self.left_walls:
+            if self.environment_rect.colliderect(left_wall):
+                self.velocity_x = -self.horizontal_speed
+
+        for right_wall in self.right_walls:
+            if self.environment_rect.colliderect(right_wall):
+                self.velocity_x = self.horizontal_speed
 
         self.check_mario_collision()
 
-        self.mask = pygame.mask.from_surface(self.image)
-        self.rect.center = (self.centerx, self.centery)
-
-        self.mask = pygame.mask.from_surface(self.image)
-
         self.blitme()
-        self.mask = pygame.mask.from_surface(self.image)
+
+    def update_animation(self):
+        self.delta_time = pygame.time.get_ticks() - self.last_frame_ticks
+        self.last_frame_ticks = pygame.time.get_ticks()
+
+        self.animation_timer -= self.delta_time
+        if self.animation_timer <= 0:
+            self.animation_timer += self.animation_timer_length
+
+            self.current_animation_frame += 1
+            if self.current_animation_frame > len(self.image_frames) - 1:
+                self.current_animation_frame = 0
+
+            self.current_image = self.image_frames[self.current_animation_frame]
 
     def check_mario_collision(self):
-        collisions = pygame.sprite.collide_rect(self, self.mario)
-        if collisions:
-            print("Koopa got me")
+        if self.mario.rect.colliderect(self.top_rect):
+            self.mario.gui.score += 250
+            self.mario.gui.update_score_text()
+            self.mario.vel.y = -1
+            self.kill()
+        if self.mario.rect.colliderect(self.left_rect) or self.mario.rect.colliderect(self.right_rect):
+            print("haha")
 
     def blitme(self):
-        self.screen.blit(self.image, self.rect)
-        pygame.draw.rect(self.screen, (255, 0, 0), self.rect, 1)
+        if self.velocity_x > 0:
+            self.screen.blit(pygame.transform.flip(self.current_image, True, False), self.rect)
+        else:
+            self.screen.blit(self.current_image, self.rect)
 
-    def get_mask(self):
-        return self.mask
+        # Draw debug hitboxes
+        pygame.draw.rect(self.screen, (0, 0, 255), self.rect, 1)
+        pygame.draw.rect(self.screen, (0, 255, 255), self.environment_rect, 1)
+        pygame.draw.rect(self.screen, (0, 255, 0), self.top_rect, 1)
+        pygame.draw.rect(self.screen, (255, 0, 0), self.left_rect, 1)
+        pygame.draw.rect(self.screen, (255, 0, 0), self.right_rect, 1)
